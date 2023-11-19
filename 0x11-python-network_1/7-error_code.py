152b84164f60b41345502ea4b5dbb076b9510bb8
Hamza Kenbouch <125465657+Hamzakenbouch@users.noreply.github.com>
2023-11-19 16:14:59 +0100

    update

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" send a request to and display the response body """
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    if r.status_code >= 400:
+        print(f"Error code:", r.status_code)
+    else:
+        print(r.text)
