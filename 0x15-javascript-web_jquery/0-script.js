237616140b3d3fc6435f271afe5b5c7b9c070a3f
Hamza Kenbouch <hamzakenbouch.co@gmail.com>
2024-01-10 10:02:49 -0800

    delete

--- 0x15-javascript-web_jquery/0-script.js
+++
@@ -1,4 +0,0 @@
-// Select the <header> element
-const headerElement = document.querySelector('header');
-// Update the text color to red
-headerElement.style.color = '#FF0000';
--- 0x15-javascript-web_jquery/9-script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-  $.get('https://hellosalut.stefanbohacek.dev/?lang=fr', function (data) {
-    const helloTranslation = data.hello;
-    $('#hello').text(helloTranslation);
-  });
-});
